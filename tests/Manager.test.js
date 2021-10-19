b521db8eb06665a59d6a85d0852f2bf7fa217983
Cole Cochran <colecochran405@gmail.com>
2021-10-18 19:34:52 -0500

    creating manager test and profile

---
+++ tests/Manager.test.js
@@ -0,0 +1,20 @@
+const Manager = require("../lib/Manager");
+//const { describe } = require("@jest/globals");
+
+const manager = new Manager('Anakin Skywalker', 1, 'darth.vader@starwars.com', 66);
+describe('biggest mikes office number',()=>{
+it( "big mikes office number", () => {
+   //const Number = 66;
+   const manager = new Manager( "Anakin Skywalker", 1, "darth.vader@starwars.com", 66 );
+
+   expect( manager.officeNumber ).toEqual( 66 );
+})
+})
+describe('test theRole()',()=>{
+it( "see if theRole() returns \"Manager\" ", () => {
+   //const Role = "Manager";
+   const manager = new Manager( "Anakin Skywalker", 1, "darth.vader@starwars.com", 66 );
+
+   expect( manager.theRole()).toEqual( "Manager" );
+})
+})
